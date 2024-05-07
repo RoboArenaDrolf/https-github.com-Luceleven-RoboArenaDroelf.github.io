a53f72e0844ba4454d2355731656a9e5cd487ee7
Luca Dürr <luca.duerr@student.uni-tuebingen.de>
2024-05-07 16:20:36 +0200

    Rewrote game.py to run a basic version of the robo arena game.

--- game.py
+++ game.py
@@ -1,74 +1,67 @@
 import pygame
-import random
+# import random
 import sys
-from screeninfo import get_monitors
+# from screeninfo import get_monitors
+
+from robot import Robot
+from movement import Movement
+from arena import Arena
 
 pygame.init()
 
-monitor = get_monitors()[0]
-windowWidth = monitor.width
-windowHeight = monitor.height
+# # Commenting these out for now, maybe introduce them later for full-screen support
+# monitor = get_monitors()[0]
+# windowWidth = monitor.width
+# windowHeight = monitor.height
 
-screen = pygame.display.set_mode((windowWidth, windowHeight))
-pygame.display.set_caption("SNAKE")
+arena_size = 1000
 
-x = windowWidth // 2
-y = windowHeight - windowHeight // 10
+screen = pygame.display.set_mode((arena_size, arena_size))
+pygame.display.set_caption("Robo Arena")
 
-# Farbdefinitionen
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (200, 0, 0)
 
-# Uhr definieren
-clock = pygame.time.Clock()
+# # Event-Timer für die Erzeugung der Quadrate
+# ADD_SQUARE = pygame.USEREVENT + 1
+# pygame.time.set_timer(ADD_SQUARE, 1000) # Event alle 1000 Millisekunden
 
-# Quadratgröße
-square_size = windowHeight // 40
-
-# Liste für die Positionen und Geschwindigkeiten der schwarzen Quadrate
-black_squares = []
-white_squares = []
-
-# Punktezähler
-points = 0
-
-# Event-Timer für die Erzeugung der Quadrate
-ADD_SQUARE = pygame.USEREVENT + 1
-pygame.time.set_timer(ADD_SQUARE, 1000)  # Event alle 1000 Millisekunden
-
-# Rechtecke für die Schaltflächen "Nochmal spielen" und "Spiel beenden"
-retry_rect = pygame.Rect(0, 0, 0, 0)
+# Rechtecke für die Schaltflächen "Resume" und "Quit Game"
+resume_rect = pygame.Rect(0, 0, 0, 0)
 quit_rect = pygame.Rect(0, 0, 0, 0)
 
-def game_over_screen():
-    global retry_rect, quit_rect
-    # "Game Over" anzeigen
-    font = pygame.font.Font(None, 64)
-    text = font.render("Game Over", True, black)
-    screen.blit(text, (windowWidth // 2 - text.get_width() // 2, windowHeight // 2 - text.get_height() // 2))
 
-    # "Nochmal spielen" und "Spiel beenden" anzeigen
+def pause_screen():
+    global resume_rect, quit_rect
+    # "Paused Game" anzeigen
+    font = pygame.font.Font(None, 64)
+    text = font.render("Paused Game", True, black)
+    screen.blit(text, (arena_size // 2 - text.get_width() // 2, arena_size // 2 - text.get_height() // 2))
+
+    # "Resume" und "Quit Game" anzeigen
     font = pygame.font.Font(None, 36)
-    text_retry = font.render("Nochmal spielen", True, black)
-    text_quit = font.render("Spiel beenden", True, black)
+    text_retry = font.render("Resume", True, white)
+    text_quit = font.render("Quit Game", True, white)
 
     # Positionen der Texte festlegen
-    retry_rect = text_retry.get_rect(center=(windowWidth // 2, windowHeight // 2 + 50))
-    quit_rect = text_quit.get_rect(center=(windowWidth // 2, windowHeight // 2 + 100))
+    resume_rect = text_retry.get_rect(center=(arena_size // 2, arena_size // 2 + 50))
+    quit_rect = text_quit.get_rect(center=(arena_size // 2, arena_size // 2 + 100))
 
     # Rechtecke zeichnen
-    pygame.draw.rect(screen, white, retry_rect, 2)
-    pygame.draw.rect(screen, white, quit_rect, 2)
+    pygame.draw.rect(screen, black, resume_rect)
+    pygame.draw.rect(screen, black, quit_rect)
 
     # Texte zeichnen
-    screen.blit(text_retry, retry_rect)
+    screen.blit(text_retry, resume_rect)
     screen.blit(text_quit, quit_rect)
 
     pygame.display.update()
 
-game_over = False
+robot = Robot(300, 100, 25, 45)
+movement = Movement()
+arena = Arena()
 
+game_paused = False
 run = True
 while run:
     pygame.time.delay(20)
@@ -76,77 +69,55 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        elif event.type == ADD_SQUARE and not game_over:
-            # Zufällige X-Position generieren und zur Liste hinzufügen
-            random_x_position = random.randint(0, windowWidth - square_size)
-            black_squares.append([random_x_position, 0, 5])  # [x, y, speed]
-            random_x_position = random.randint(0, windowWidth - square_size)
-            white_squares.append([random_x_position, 0, 5])
+        # elif event.type == ADD_SQUARE and not game_over:
+        #     # Zufällige X-Position generieren und zur Liste hinzufügen
+        #     random_x_position = random.randint(0, windowWidth - square_size)
+        #     black_squares.append([random_x_position, 0, 5])  # [x, y, speed]
+        #     random_x_position = random.randint(0, windowWidth - square_size)
+        #     white_squares.append([random_x_position, 0, 5])
 
-        elif event.type == pygame.MOUSEBUTTONDOWN and game_over:
+        elif event.type == pygame.MOUSEBUTTONDOWN and game_paused:
             # Mausklick-Ereignis verarbeiten, wenn das Spiel vorbei ist
             mouse_pos = pygame.mouse.get_pos()
-            if retry_rect.collidepoint(mouse_pos):
-                # Nochmal spielen
-                game_over = False
-                black_squares.clear()
-                white_squares.clear()
-                x = windowWidth // 2
-                y = windowHeight - windowHeight // 10
-                points = 0
+            if resume_rect.collidepoint(mouse_pos):
+                # Weiter spielen
+                game_paused = False
             elif quit_rect.collidepoint(mouse_pos):
                 # Spiel beenden
                 pygame.quit()
                 sys.exit()
 
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        game_paused = True
 
-    if not game_over:  # Das Spiel läuft nur weiter, wenn es nicht vorbei ist
-        if keys[pygame.K_RIGHT]:
-            x = min(x + 5, windowWidth - square_size)  # Spieler kann nicht über den Bildschirmrand hinaus
-        if keys[pygame.K_LEFT]:
-            x = max(x - 5, 0)  # Spieler kann nicht über den Bildschirmrand hinaus
+    if not game_paused:  # Das Spiel läuft nur weiter, wenn es nicht vorbei ist
+        # # Überprüfen, ob das rote Rechteck mit einem schwarzen Rechteck kollidiert
+        # for square in black_squares:
+        #     square[1] += square[2]  # Update y-coordinate using speed
+        #     if y < square[1] + square_size and y + playerSize > square[1] and x < square[0] + square_size and x + playerSize > square[0]:
+        #         game_paused = True
+        # # Überprüfen, ob das rote Rechteck mit einem weißen Rechteck kollidiert
+        # for square in white_squares:
+        #     square[1] += square[2]  # Update y-coordinate using speed
+        #     if y < square[1] + square_size and y + 50 > square[1] and x < square[0] + square_size and x + 50 > square[0]:
+        #         #update des Punktecounters
+        #         points = points+1
+        #         #weißes Rechteck wird außerhalb des spielbereichs gepusht
+        #         square[1] = square[1] + 200
 
-        playerSize = windowHeight // 16
+        arena.paint_arena(pygame, screen)
+        movement.move_robot(robot, arena_size)
+        robot.paint_robot(pygame, screen)
+    else:
+        pause_screen()
 
-
-        # Überprüfen, ob das rote Rechteck mit einem schwarzen Rechteck kollidiert
-        for square in black_squares:
-            square[1] += square[2]  # Update y-coordinate using speed
-            if y < square[1] + square_size and y + playerSize > square[1] and x < square[0] + square_size and x + playerSize > square[0]:
-                game_over = True
-        # Überprüfen, ob das rote Rechteck mit einem weißen Rechteck kollidiert
-        for square in white_squares:
-            square[1] += square[2]  # Update y-coordinate using speed
-            if y < square[1] + square_size and y + 50 > square[1] and x < square[0] + square_size and x + 50 > square[0]:
-                #update des Punktecounters
-                points = points+1
-                #weißes Rechteck wird außerhalb des spielbereichs gepusht
-                square[1] = square[1] + 200
-
-        screen.fill((0, 255, 200))
-
-    # Schwarze Quadrate aktualisieren und zeichnen
-        for square in black_squares:
-            pygame.draw.rect(screen, black, [square[0], square[1], square_size, square_size])
-    # Weiße Quadrate aktualisieren und zeichnen
-        for square in white_squares:
-            pygame.draw.rect(screen, white, [square[0], square[1], square_size, square_size])
-
-    # Spielerrechteck zeichnen
-
-    pygame.draw.rect(screen, red, (x, y, playerSize, playerSize))
-
-
-    if game_over:
-        game_over_screen()
-
-    # Text für den Punket-counter
-    font = pygame.font.Font(None, 64)
-    text_points = font.render(f"Deine Punkte {points} " , True, black)
-    # Position des Counters
-    points_rect = text_points.get_rect(center=(200 , 50))
-    screen.blit(text_points, points_rect)
+    # # Text für den Punkte-counter
+    # font = pygame.font.Font(None, 64)
+    # text_points = font.render(f"Deine Punkte {points} " , True, black)
+    # # Position des Counters
+    # points_rect = text_points.get_rect(center=(200 , 50))
+    # screen.blit(text_points, points_rect)
 
     pygame.display.update()
 
