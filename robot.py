6f6ecf6d3fb22f276e343ed1e531b1b108f7adaf
bjoern <bjoernxx47@gmail.com>
2024-05-16 18:12:20 +0200

    added changes to robotclass missing in last commit

--- robot.py
+++ robot.py
@@ -6,12 +6,38 @@ class Robot:
     posy: int
     radius = 0
     alpha = 0
+    accel = 0
+    accel_max = 1
+    accel_alpha = 0     # this might just be useless for us
+    accel_alpha_max = 1  # this feels VERY useless
+    vel = 0
+    vel_alpha = 0
 
-    def __init__(self, x, y, r, a):
+    def __init__(self, x, y, r, a, am, aam):
         self.posx = x
         self.posy = y
         self.radius = r
         self.alpha = a % 360  # thanks to mod 360 this will no longer break
+        self.accel_max = am
+        self.accel_alpha_max = aam
+
+    def change_acceleration(self, a):
+        if a < self.accel_max:
+            self.accel = a
+        else:
+            self.accel = self.accel_max
+
+    def change_rot_acceleration(self, aa):
+        if aa < self.accel_alpha_max:
+            self.accel_alpha = aa
+        else:
+            self.accel_alpha = self.accel_alpha_max
+
+    def change_velocity(self, v):
+        self.vel = v
+
+    def change_turn_velocity(self, va):
+        self.vel = va
 
     def paint_robot(self, pygame, screen):
         pygame.draw.circle(screen, "blue", (self.posx, self.posy), self.radius)
