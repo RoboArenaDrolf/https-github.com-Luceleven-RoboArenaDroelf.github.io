84665aa489e67b9b91a5fbc3cd9959eed8b21f4d
Manu <emanuelgan@yahoo.de>
2024-05-28 15:12:31 +0200

    added horizontal block physics and borders for arena-size

--- movement.py
+++ movement.py
@@ -1,20 +1,42 @@
 import pygame
 
-
 class Movement:
     gravity = 0.5  # Schwerkraftkonstante
     vertical_speed = 0  # Anfangsgeschwindigkeit in der vertikalen Richtung
 
-    def move_robot(self, robot, screen_height, x):
+    def move_robot(self, robot, screen_height, screen_width, x, arena):
         keys = pygame.key.get_pressed()
         robot.posx += x
 
-        if keys[pygame.K_UP]:
+        # Überprüfen, ob der Roboter die seitlichen Grenzen der Arena erreicht hat
+        if robot.posx < robot.radius:
+            robot.posx = robot.radius
+        elif robot.posx > screen_width - robot.radius:
+            robot.posx = screen_width - robot.radius
+
+        # Tastatureingaben verarbeiten
+        if keys[pygame.K_UP] and self.on_ground(robot, arena):
             self.vertical_speed = -10  # Vertikale Geschwindigkeit für Sprung setzen
 
-        # Vertikale Bewegung mit Schwerkraft
+        # Vertikale Bewegung
         self.vertical_speed += self.gravity
-        robot.posy += self.vertical_speed
+
+        # Überprüfen, ob der Roboter die oberen und unteren Grenzen der Arena erreicht hat
+        if robot.posy - robot.radius < 0:
+            robot.posy = robot.radius
+            if self.vertical_speed < 0:
+                self.vertical_speed = 0
+        elif robot.posy + robot.radius > screen_height:
+            robot.posy = screen_height - robot.radius
+            if self.vertical_speed > 0:
+                self.vertical_speed = 0
+        else:
+            robot.posy += self.vertical_speed
+
+        # Kollision mit dem Boden überprüfen
+        if self.check_collision(robot, arena):
+            robot.posy = ((robot.posy // arena.tile_size) + 1) * arena.tile_size - robot.radius
+            self.vertical_speed = 0
 
         # Grenzen für die vertikale Position (optional)
         if robot.posy > screen_height:
@@ -22,3 +44,17 @@ class Movement:
             self.vertical_speed = 0
         elif robot.posy < 0:
             robot.posy = 0
+
+
+    def on_ground(self, robot, arena):
+        # Überprüfen, ob der Roboter auf dem Boden steht
+        x = int(robot.posx // arena.tile_size)
+        y = int((robot.posy + robot.radius) // arena.tile_size)
+        return arena.is_solid(x, y) or self.vertical_speed > 0
+
+
+    def check_collision(self, robot, arena):
+        # Überprüfen, ob der Roboter mit einem festen Tile kollidiert
+        x = int(robot.posx // arena.tile_size)
+        y = int((robot.posy + robot.radius) // arena.tile_size)
+        return arena.is_solid(x, y)
--- robot.py
+++ robot.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Robot:
     posx: int
     posy: int
@@ -53,8 +52,6 @@ class Robot:
 
     def paint_robot(self, pygame, screen):
         pygame.draw.circle(screen, "blue", (self.posx, self.posy), self.radius)
-        # now to get the orientation
         new_x = self.radius * (math.cos(self.alpha))
         new_y = self.radius * (math.sin(self.alpha))
-        pygame.draw.line(screen, "black", (self.posx, self.posy),
-                         (self.posx+new_x, self.posy+new_y))
+        pygame.draw.line(screen, "black", (self.posx, self.posy), (self.posx+new_x, self.posy+new_y))
