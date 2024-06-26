9419916c1c4399f639ad5bf12df84c2c2a38c4ce
Luceleven <lucaduerr@web.de>
2024-06-26 15:46:42 +0200

    Fixed bug of game pausing when switching to fullscreen.

--- src/game.py
+++ src/game.py
@@ -411,7 +411,9 @@ while run:
     current_window_pos = window.position
     if playing:
         if current_window_pos != initial_window_pos:
-            game_paused = True
+            flags = pygame.display.get_surface().get_flags()
+            if not bool(flags & pygame.FULLSCREEN):
+                game_paused = True
             initial_window_pos = current_window_pos
 
     for event in pygame.event.get():
